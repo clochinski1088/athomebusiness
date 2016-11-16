34d55f63bfb1447066c905d7c45270c059828d9e
Collin Lochinski <collinlochinski@Collins-MacBook-Air.local>
2016-11-15 20:18:08 -0500

    This is first commit from MacBook air

---
+++ Lularoe/DashboardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lularoe
+//
+//  Created by Collin Lochinski on 11/14/16.
+//  Copyright © 2016 Collin Lochinski. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
